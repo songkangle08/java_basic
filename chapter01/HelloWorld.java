4c2c13f646638f918c2914a109f8d99350a3106f
songkangle <sklhtml@163.com>
2023-12-05 08:08:28 +0800

    Hello World

---
+++ chapter01/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * ClassName：HelloWorld,
+ * Package: PACKAGE_NAME,
+ * Desc:,
+ * @Create: 2023/12/5 7:54,
+ * @Author: skl,
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!,你好，中国！");
+    }
+}
