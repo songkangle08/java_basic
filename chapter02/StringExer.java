9855006334c6494cae1708a43b7adb4924490ba6
songkangle <sklhtml@163.com>
2023-12-10 22:43:36 +0800

    字符串

---
+++ chapter02/StringExer.java
@@ -0,0 +1,15 @@
+/*
+  要求填写自己的姓名，年龄，性别，体重，婚姻状态，练习方式等等。
+*/ 
+public StringTest{
+  public static void main(String[] args){
+    
+    String name = "skl";
+    int age = 28;
+    char gender = '男';
+    double weight = 130.5;
+    boolean isMarried = true;
+    String phoneNumber = "XXXXXXXXXXX";
+
+  }
+} 
