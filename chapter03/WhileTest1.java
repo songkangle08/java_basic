950f74608c98b48a39fed13a543232a94a09c118
songkangle <44560125+songkangle08@users.noreply.github.com>
2024-02-27 10:35:34 +0800

    while-随机数

--- chapter03/WhileTest1.java
+++ chapter03/WhileTest1.java
@@ -1,11 +1,40 @@
 /*
-  提示：生成一个[a,b]范围的随机数的方式 (int)(Math.random() * (b - a + 1) + a)
-*/ 
+  随机生成一个100以内的随机数，猜这个随机数是多少？
+  从键盘输入数，如果大了，提示大了，如果小了，提示小了，如果对了，就不在猜了，并统计一共猜了多少次
+  提示：生成一个[a,b]范围的随机数的方式(int)(Math.random() * (b-a+1) +a)
+*/
 
-class WhileTest1{
-  public static void main(String[] args){
+import java.util.Scanner;
 
-    int target = (int)(Math.random() * 100 + 1)
+public class WhileTest1 {
+  public static void main(String[] args) {
+    // 1. 生成一个随机数
+    int random = (int) (Math.random() * (100 - 0 + 1) + 0);
+
+    Scanner scan = new Scanner(System.in);
+
+    boolean flag = true;
+    int count = 0;
+
+    while (flag) {
+      System.out.print("请输入100以内的数：");
+      int guess = scan.nextInt();
+
+      if (guess > random) {
+        System.out.println("大了");
+      } else if (guess < random) {
+        System.out.println("小了");
+      } else {
+        System.out.println("恭喜你，猜对了");
+        flag = false;
+      }
+      count++;
+    }
+    System.out.println("你总共猜了" + count + "次");
+    scan.close();
 
   }
+
+}
+
 }
