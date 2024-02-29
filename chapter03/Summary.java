27a302f3b68406efe09c0da7d73a1bc3b5e1074a
songkangle <sklhtml@163.com>
2024-02-29 22:48:25 +0800

    流程控制总结

---
+++ chapter03/Summary.java
@@ -0,0 +1,42 @@
+/*
+  流程控制结构：
+    - 顺序结构
+    - 分支结构
+      - if-lese
+      - switch-case
+    - 循环结构
+      - for
+      - while
+      - do-while
+  分支结构之： if-else
+    - 在程序中，凡是遇到了需要使用分支结构的地方，都可以考虑使用if-else
+  分支结构之：switch-case
+    - 在特殊的场景下，分支结构可以考虑switch-case
+      - 指定的数据类型： byte short char int 枚举 String
+      - 可以考虑的常量值有限且取值情况不多
+    - 特别之处：case穿透。
+    - 在能使用switch-case的情况下，推进使用switch-case，因为比if-else效率更高。
+  循环结构之1：for
+    - 凡是循环结构，都有4个要素：1：初始化条件 2：循环条件（boolean类型） 3：循环体  4： 迭代条件
+    - 应用场景，有明确的遍历次数。
+  循环结构之2：while
+    - 应用场景：没有明确的遍历次数
+  循环结构之3：do-while
+    - 至少会执行一次循环体
+    - 开发中，应用较少
+  “无限循环”
+
+  嵌套循环
+
+  关键字break、continue
+
+  Sacnner
+  
+*/ 
+
+
+public Summary{
+  public static void main(String args[]){
+
+  }
+}
