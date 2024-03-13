96900b211502807cd273560f3646d20ee56e6e52
songkangle <44560125+songkangle08@users.noreply.github.com>
2024-03-13 16:47:53 +0800

    数组联系-星期几

--- chapter05/src/com/atguigu1/one/ArrayExer2.java
+++ chapter05/src/com/atguigu1/one/ArrayExer2.java
@@ -3,7 +3,8 @@ package com.atguigu1.one;
 import java.util.Scanner;
 
 /*
-* 案例：
+* 案例：输出英文星期几
+  用一个数组，保存星期一到星期天的7个英语单词，从键盘输入1-7，显示对应的单词{"Monday" ,"Tuesday" , "Wednesday" ,"Thursday","Friday" ,"Saturday" ,"Sunday"}
 * */
 public class ArrayExer2 {
     public static void main(String[] args){
